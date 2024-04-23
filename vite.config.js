13ddf00f186106c35d387334a5c9b56c60af3fe2
AHMEDBAKAR33 <ahmedbakar37@yahoo.com>
2024-04-23 14:14:41 +0200

    Add Breeze packages and modify postcss file for suitable code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            output: 'public', // Output assets directly into the 'public' folder
             refresh: true,
         }),
     ],
